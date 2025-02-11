2c011191fab06b7ed5f04945622a3ffb9e1fc96d
Radu_Grig00 <grigorasradu40@gmail.com>
2025-02-11 20:36:57 +0100

    primo commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import { AboutComponent } from './about/about.component';
+import { GalleriaComponent } from './galleria/galleria.component';
+import { ContattiComponent } from './contatti/contatti.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'galleria', component: GalleriaComponent},
+  { path: 'contatti', component: ContattiComponent},
+  { path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { NavComponent } from './nav/nav.component';
+import { AboutComponent } from './about/about.component';
+import { GalleriaComponent } from './galleria/galleria.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ContattiComponent } from './contatti/contatti.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    NavComponent,
+    AboutComponent,
+    GalleriaComponent,
+    NotFoundComponent,
+    ContattiComponent
   ],
   imports: [
     BrowserModule,
